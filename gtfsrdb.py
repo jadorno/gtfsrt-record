c52e31784fa2bf9847d04019c67eb3f3babadfa0
Jennifer Adorno <jadorno@me.com>
2018-02-21 18:25:11 -0500

    Implemented API Arguments

--- gtfsrdb.py
+++ gtfsrdb.py
@@ -9,23 +9,62 @@ from google.protobuf.json_format import MessageToJson
 sys.path.insert(0, 'protobuf')
 import gtfs_realtime_pb2 as gtfsrt
 
-with open(sys.argv[1]) as config_file:
-        config = json.load(config_file)
 
-if not config['database_upload'] and not config['proto_download']:
+config = {}
+
+if os.environ.get('API_URL') is not None:
+	r = requests.get(os.environ.get('API_URL')+"/status/gtfsrt-config")
+	config['DB_URL'] = os.environ.get('DB_URL', r.json()['DB_URL'])
+	config['DB_UPLOAD'] = os.environ.get('DB_UPLOAD', r.json()['DB_UPLOAD'])
+	config['PB_DOWNLOAD'] = os.environ.get('PB_DOWNLOAD', r.json()['PB_DOWNLOAD'])
+	config['PB_PATH'] = os.environ.get('PB_PATH', r.json()['PB_PATH'])
+	config['SLEEP_TIME'] = os.environ.get('SLEEP_TIME', r.json()['SLEEP_TIME'])
+	config['SLEEP_ADAPTIVE'] = os.environ.get('SLEEP_ADAPTIVE', r.json()['SLEEP_ADAPTIVE'])
+	config['URL_TRIP_UPDATES_ENABLED'] = os.environ.get('URL_TRIP_UPDATES_ENABLED', r.json()['URL_TRIP_UPDATES_ENABLED'])
+	config['URL_TRIP_UPDATES'] = os.environ.get('URL_TRIP_UPDATES', r.json()['URL_TRIP_UPDATES'])
+	config['URL_VEHICLE_POSITIONS_ENABLED'] = os.environ.get('URL_VEHICLE_POSITIONS_ENABLED', r.json()['URL_VEHICLE_POSITIONS_ENABLED'])
+	config['URL_VEHICLE_POSITIONS'] = os.environ.get('URL_VEHICLE_POSITIONS', r.json()['URL_VEHICLE_POSITIONS'])
+	config['URL_ALERTS_ENABLED'] = os.environ.get('URL_ALERTS_ENABLED', r.json()['URL_ALERTS_ENABLED'])
+	config['URL_ALERTS'] = os.environ.get('URL_ALERTS', r.json()['URL_ALERTS'])
+else:
+	config['DB_URL'] = os.environ.get('DB_URL')
+	config['DB_UPLOAD'] = os.environ.get('DB_UPLOAD')
+	config['PB_DOWNLOAD'] = os.environ.get('PB_DOWNLOAD')
+	config['PB_PATH'] = os.environ.get('PB_PATH')
+	config['SLEEP_TIME'] = os.environ.get('SLEEP_TIME')
+	config['SLEEP_ADAPTIVE'] = os.environ.get('SLEEP_ADAPTIVE')
+	config['URL_TRIP_UPDATES_ENABLED'] = os.environ.get('URL_TRIP_UPDATES_ENABLED')
+	config['URL_TRIP_UPDATES'] = os.environ.get('URL_TRIP_UPDATES')
+	config['URL_VEHICLE_POSITIONS_ENABLED'] = os.environ.get('URL_VEHICLE_POSITIONS_ENABLED')
+	config['URL_VEHICLE_POSITIONS'] = os.environ.get('URL_VEHICLE_POSITIONS')
+	config['URL_ALERTS_ENABLED'] = os.environ.get('URL_ALERTS_ENABLED')
+	config['URL_ALERTS'] = os.environ.get('URL_ALERTS')
+
+if not config['DB_UPLOAD'] == 'true' and not config['PB_DOWNLOAD'] == 'true':
 	print("You have Protobuffer Download and Database Upload Disabled.")
 	print("The program doesn't do anything else.")
 	exit()
 
-if config['database_upload']:
+sleep_time = int(config['SLEEP_TIME'])
+gtfsrt_enabled = []
+gtfsrt_url = {}
+if config['URL_TRIP_UPDATES_ENABLED'] == 'true':
+	gtfsrt_enabled.append('trip_updates')
+	gtfsrt_url['trip_updates'] = config['URL_TRIP_UPDATES']
+if config['URL_VEHICLE_POSITIONS_ENABLED'] == 'true':
+	gtfsrt_enabled.append('vehicle_positions')
+	gtfsrt_url['vehicle_positions'] = config['URL_VEHICLE_POSITIONS']
+if config['URL_ALERTS_ENABLED'] == 'true':
+	gtfsrt_enabled.append('alerts')
+	gtfsrt_url['alerts'] = config['URL_ALERTS']
+
+if config['DB_UPLOAD'] == 'true':
 	import pymongo
 
-	if os.environ.get('DB_URL') != None:
-		config['conn_url'] = os.environ.get('DB_URL')
-	client = pymongo.MongoClient(config['conn_url'])
-	db = client[config['conn_db']]
+	client = pymongo.MongoClient(config['DB_URL'])
+	db = client.get_database()
 
-	for table_name in config['gtfsrt_enabled']:
+	for table_name in gtfsrt_enabled:
 		timestamp_index_exist = False
 		if table_name in db.collection_names():
 			index_info = db[table_name].index_information()
@@ -43,54 +82,54 @@ if config['database_upload']:
 
 while True:
 
-	increaseSleep = False
+	increase_sleep = False
 
-	for table_name in config['gtfsrt_enabled']:
+	for table_name in gtfsrt_enabled:
 
 		try:
 
-			r = requests.get(config[table_name+'_url'], timeout=10)
+			r = requests.get(gtfsrt_url[table_name], timeout=10)
 			fm = gtfsrt.FeedMessage()
 			fm.ParseFromString(r.content)
 			data = json.loads(MessageToJson(fm))
 
-			if config['proto_download']:
-				outputFile = config['proto_path']+'/'+data['header']['timestamp']+"_"+table_name+".pb"
+			if config['PB_DOWNLOAD'] == 'true':
+				outputFile = config['PB_PATH']+'/'+data['header']['timestamp']+"_"+table_name+".pb"
 				if os.path.isfile(outputFile):
 					print(str(data['header']['timestamp']),"- Protobuf File Duplicate on "+outputFile)
-					increaseSleep = True				
+					increase_sleep = True				
 				f = open(outputFile, 'wb')
 				f.write(r.content)
 				f.close()
 				print(data['header']['timestamp'],"- Protobuf File Written on "+outputFile)
 
-			if config['database_upload']:
+			if config['DB_UPLOAD'] == 'true':
 				data['header']['timestamp'] = int(data['header']['timestamp'])
 				try:
 					db[table_name].insert_one(data)
 					print(str(data['header']['timestamp']),"- DB Inserted to "+table_name+".")
 				except pymongo.errors.DuplicateKeyError:
 					print(str(data['header']['timestamp']),"- DB Rejected to "+table_name+". Duplicate Keys.")
-					increaseSleep = True
+					increase_sleep = True
 
 		except requests.exceptions.ReadTimeout as e:
-			print("Connection Error to: "+config[table_name+'_url'])
+			print("Connection Error to: "+gtfsrt_url[table_name])
 			print(e)
 		except requests.exceptions.ConnectionError as e:
-			print("Connection Error to: "+config[table_name+'_url'])
+			print("Connection Error to: "+gtfsrt_url[table_name])
 			print(e)
 		except requests.exceptions.ChunkedEncodingError as e:
-			print("Connection Error to: "+config[table_name+'_url'])
+			print("Connection Error to: "+gtfsrt_url[table_name])
 			print(e)
 		except DecodeError:
-			print("Unable to decode: "+config[table_name+'_url'])
+			print("Unable to decode: "+gtfsrt_url[table_name])
 		except KeyError as e:
-			print("Missing Value in Protobuffer from: "+config[table_name+'_url'])
+			print("Missing Value in Protobuffer from: "+gtfsrt_url[table_name])
 			print(e)
 
-	if config['adaptive_sleep'] and increaseSleep:
-		config['sleep_time'] = config['sleep_time'] + 5
-		print("Increased Sleep Time to "+str(config['sleep_time']))
+	if config['SLEEP_ADAPTIVE'] == 'true' and increase_sleep:
+		sleep_time = sleep_time + 5
+		print("Increased Sleep Time to "+str(sleep_time))
 	else: 
-		print("Sleeping for "+str(config['sleep_time']))
-	time.sleep(config['sleep_time'])
+		print("Sleeping for "+str(sleep_time))
+	time.sleep(sleep_time)
